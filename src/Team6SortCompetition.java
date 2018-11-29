0ee062aefb6237a7a18ebb2b91d367c5ec416055
catw101 <catelenwu@gmail.com>
2018-11-29 11:35:59 -0500

    Update Team6SortCompetition.java

--- src/Team6SortCompetition.java
+++ src/Team6SortCompetition.java
@@ -38,7 +38,24 @@ public class Team6SortCompetition extends SortCompetition {
     }
 
     public int challengeFour(int[][] arr){
-        return 0;
+        public int challengeFour(int[][] arr){
+        int[] medians = new int[1000];
+        for (int i = 0; i < arr.length; i++) {
+            Utilities.quickSort(arr[i], 0, arr[i].length-1);
+            if(arr[i].length % 2 == 1){
+                int idx = (int)Math.floor(arr[i].length / 2);
+                return arr[i][idx];
+                medians[i] = arr[i][idx];
+            }else{
+                int idx = (int)Math.floor(arr[i].length /2);
+                int idx2 = idx - 1;
+                int median = (int)Math.floor((arr[i][idx] + arr[i][idx2]) / 2);
+                return median;
+                medians[i] = median;
+            }
+        }
+        Utilities.quickSort(medians, 0, medians.length-1);
+        
     }
 
     public int challengeFive(Comparable[] arr, Comparable query){
