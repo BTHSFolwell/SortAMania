2d40810e934327d7873236dc8b5dee7d010f52da
BT_1N3_09 <cstanilovsky9605@bths.edu>
2018-11-29 10:36:31 -0500

    Update

---
+++ src/Team6SortCompetition.java
@@ -0,0 +1,25 @@
+public class Team6SortCompetition extends SortCompetition {
+
+    public int challengeOne(int[] arr){
+
+    }
+    public int challengeTwo(String[] arr, String query){
+
+    }
+
+    public int challengeThree(int[] arr){
+
+    }
+
+    public int challengeFour(int[][] arr){
+
+    }
+
+    public int challengeFive(Comparable[] arr, Comparable query){
+
+    }
+
+    public String greeting(){
+
+    }
+}
