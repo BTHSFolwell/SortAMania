8896fc9dc77829fd4abec39055af818926c468d0
BT_1N3_04 <cchau3846@bths.edu>
2018-11-30 11:28:20 -0500

    challenge one

--- src/Runner.java
+++ src/Runner.java
@@ -1,11 +1,26 @@
 public class Runner {
     public static void main(String args[]){
         Team4SortCompetition sorter = new Team4SortCompetition();
+        int[] randIntArr = sorter.randomIntsArr(10000);
 
         //Challenge One
-        int[] one = sorter.randomIntsArr(10000);
+        System.out.println("Unsorted");
+        printIntArr(randIntArr);
+
         long time = System.currentTimeMillis();
-        sorter.challengeOne(one);
+        sorter.challengeOne(randIntArr);
         time = System.currentTimeMillis()-time;
+        System.out.println("Challenge One Time Taken: " + (time * 0.001) + " seconds");
+
+        System.out.println("Sorted");
+        printIntArr(randIntArr);
+    }
+
+    public static int[] printIntArr(int[] arr){
+        for(int x = 0; x < arr.length; x++){
+            System.out.print(arr[x] + " ");
+        }
+        System.out.print("\n");
+        return null;
     }
 }
--- src/Team4SortCompetition.java
+++ src/Team4SortCompetition.java
@@ -1,14 +1,41 @@
 public class Team4SortCompetition extends SortCompetition{
     //Challenge One
     @Override
-    public int challengeOne(int[] arr) { //Standard sort and process--Mergesort
+    public int challengeOne(int[] arr) { //Standard sort and process--Quicksort
+        quickSort(arr,0,9999);
         return 0;
+    }
+
+    public void quickSort(int[] arr, int low, int high){
+        if(low< high){
+            int p = partition(arr,low, high);
+
+            quickSort(arr,low, p - 1);
+            quickSort(arr, p+1, high);
+        }
+    }
+
+    public int partition( int[] arr, int  left, int right){
+        int pivot = arr[right];
+        int i = left - 1;
+        for(int j = left; j <right; j++){
+            if(arr[j] <=pivot){
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp2 = arr[i+1];
+        arr[i+1] = arr[right];
+        arr[right] = temp2;
+        return i+1;
     }
 
     public static int[] randomIntsArr(int num){
         int []arr = new int[num];
         for(int i =0; i <arr.length; i++){
-            arr[i] = (int)(Math.random()*1000);
+            arr[i] = (int)(Math.random()*10000);
         }
         return arr;
     }
