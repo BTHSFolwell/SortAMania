74bbe42d834a1c4eea5dd023e617e4ee0e4cf6f5
BT_1N3_31 <jwu8483@bths.edu>
2018-12-04 11:44:26 -0500

    fi

--- src/Team15SortCompetition.java
+++ src/Team15SortCompetition.java
@@ -4,11 +4,26 @@ public class Team15SortCompetition extends SortCompetition{
         arr[i] = arr[j];
         arr[j] = temp;
     }
+    private int part(int[] arr, int x, int y) {
+        int z = arr[y];
+        int i = x - 1;
 
-    public static void swapDouble(double[] arr, int i, int j) {
-        double temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+        for (int j = x; j < y; j ++) {
+            if (arr[j] <= z) {
+                i ++;
+                int k = arr[i]; arr[i] = arr[j]; arr[j] = k;
+            }
+        }
+        int l = arr[i + 1]; arr[i + 1] = arr[y]; arr[y] = l;
+        return i + 1;
+    }
+
+    private void quickSort(int[] arr, int x, int y) {
+        if (x < y) {
+            int z = part(arr, x, y);
+            quickSort(arr, x, z - 1);
+            quickSort(arr, z + 1, y);
+        }
     }
 
     public static void swapString(String[] arr, int i, int j) {
@@ -45,6 +60,13 @@ public class Team15SortCompetition extends SortCompetition{
             System.out.print(arr[i] + " ");
         System.out.println();
     }
+    static void printArrStr(String arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
     @Override
     public int challengeOne(int arr[]) {
         for (int i = 0; i < arr.length - 1; i++) {
@@ -62,6 +84,7 @@ public class Team15SortCompetition extends SortCompetition{
     }
     @Override
     public int challengeTwo(String[] arr, String query) {
+        int ch = 0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j].compareTo(arr[j + 1]) > 0) {
@@ -69,17 +92,41 @@ public class Team15SortCompetition extends SortCompetition{
                 }
             }
         }
-        return index;
+        for (int a = 0; a < arr.length; a++) {
+            if (arr[a].equals(query)) {
+                 ch = a;
+            } else {
+                 ch = -1;
+            }
+        }
+        return ch;
     }
 
     @Override
-    public int challengeThree(int[] arr) {
-        return 0;
+    public int challengeThree(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            if (min != i) {
+                swapInt(arr, i, min);
+            }
+        }
+        return arr[arr.length/2];
     }
 
     @Override
     public int challengeFour(int[][] arr) {
-        return 0;
+        int[] medians = new int[arr.length];
+        for (int i = 0; i < arr.length; i ++) {
+            quickSort(arr[i], 0, arr[i].length - 1);
+            medians[i] = arr[i][arr[i].length/2 - 1];
+        }
+        quickSort(medians, 0, medians.length - 1);
+        return medians[medians.length/2 - 1];
     }
 
     @Override
@@ -92,11 +139,4 @@ public class Team15SortCompetition extends SortCompetition{
         return null;
     }
 
-    public static double[] randDoubleArr(int count) {
-        double[] arr = new double[count];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] =(int) ((Math.random() * 10000.0)+1);
-        }
-        return arr;
-    }
 }
--- src/runner.java
+++ src/runner.java
@@ -2,11 +2,10 @@
 public  class runner {
     public static void main(String[] args) {
        SortCompetition Team15 = new Team15SortCompetition();
-       int[] randIntArr = randomIntsArr(10000);
-       String[] randStringArr = randomStringArr(10000,5);
+       int[] randIntArr = Team15SortCompetition.randomIntsArr(10000);
 
        System.out.println("Unsorted");
-       printArr(randIntArr);
+       Team15SortCompetition.printArr(randIntArr);
 
        long time = System.currentTimeMillis();
        int median = Team15.challengeOne(randIntArr);
@@ -15,6 +14,45 @@ public  class runner {
        System.out.println("Median equals: " + median);
 
        System.out.println("Sorted");
-       printArr(randIntArr);
+       Team15SortCompetition.printArr(randIntArr);
+       System.out.println("--------------------------------");
+       String[] randStringArr = Team15SortCompetition.randomStringArr(10000,5);
+       System.out.println("Unsorted");
+       Team15SortCompetition.printArrStr(randStringArr);
+       long time1 = System.currentTimeMillis();
+       int index = Team15.challengeTwo(randStringArr,"asdas");
+       time1 = System.currentTimeMillis() - time1;
+       System.out.println("Challenge Two Time Taken : " + time1 * 0.001 + " Seconds");
+       System.out.println("Index equals: " + index);
+
+       System.out.println("Sorted");
+       Team15SortCompetition.printArrStr(randStringArr);
+       System.out.println("--------------------------------");
+       int[] randIntArr1 = Team15SortCompetition.randomIntsArr(10000);
+
+       System.out.println("Unsorted");
+       Team15SortCompetition.printArr(randIntArr1);
+
+       long time2 = System.currentTimeMillis();
+       int median2 = Team15.challengeOne(randIntArr1);
+       time2 = System.currentTimeMillis() - time2;
+       System.out.println("Challenge One Time Taken : " + time2 * 0.001 + " Seconds");
+       System.out.println("Median equals: " + median2);
+
+       System.out.println("Sorted");
+       Team15SortCompetition.printArr(randIntArr1);
+       System.out.println("--------------------------------");
+
+       int[][] rand2DIntArr = new int[1000][1000];
+       for (int i = 0; i < rand2DIntArr.length; i ++) {
+          rand2DIntArr[i] = Team15SortCompetition.randomIntsArr(1000);
+       }
+       long time3 = System.currentTimeMillis();
+       int median3 = Team15.challengeFour(rand2DIntArr);
+       time3 = System.currentTimeMillis() - time;
+       System.out.println("Challenge Four Time Taken: " + time3 * 0.001 + " seconds.");
+       System.out.println("The median is " + median3);
+       Team15.addTime(time);
+
     }
 }
