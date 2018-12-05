be8fff956c9cf4ea6ea7a28c8ddcf0b9138b7218
kliu6151 <kliu6151@bths.edu>
2018-12-04 21:51:27 -0500

    Done

--- src/Runner.java
+++ src/Runner.java
@@ -7,77 +7,116 @@ public class Runner {
         SortCompetition team11 = new Team11SortCompetition();
 
         //Challenge One
+        System.out.println("Challenge One\n------------------------------------------");
         int[] randIntArr = randomIntsArr(10000);
 
-        System.out.println("Unsorted");
+        System.out.println("Unsorted:");
         printArr(randIntArr);
 
         long time = System.currentTimeMillis();
         int median = team11.challengeOne(randIntArr);
         time = System.currentTimeMillis() - time;
-        System.out.println("Challenge One Time Taken: " + time  + " Seconds");
+        System.out.println("\nChallenge One Time Taken: " + time  + " Seconds");
         System.out.println("Median equals: " + median);
 
-        System.out.println("Sorted");
+        System.out.println("\nSorted:");
         printArr(randIntArr);
+        System.out.println("");
 
         //Challenge Two
+        System.out.println("Challenge Two\n------------------------------------------");
         String[] randStringArr = randomStringArr(10000,5);
 
-        System.out.println("\nUnsorted:");
+        System.out.println("Unsorted:");
         printStringArr(randStringArr);
-        int indexNumber = team11.challengeTwo(randStringArr,"fasad");
+        int indexNumber = team11.challengeTwo(randStringArr,"Hello");
 
         time = System.currentTimeMillis();
         time = System.currentTimeMillis() - time;
-        System.out.println("Challenge Two Time Taken: " + time  + " Seconds");
+        System.out.println("\nChallenge Two Time Taken: " + time  + " Seconds");
         if(indexNumber > 0)
             System.out.println("Index is found at: " + indexNumber);
         else
             System.out.println("Index not found");
 
-        System.out.println("Sorted");
+        System.out.println("\nSorted:");
         printStringArr(randStringArr);
+        System.out.println("");
 
 
         //Challenge Three
+        System.out.println("Challenge Three\n------------------------------------------");
         int[] randIntArr1 = randomIntsArr(100000);
         int most = (int)(randIntArr1.length * 0.8);
         Team11SortCompetition.quickSort(randIntArr1, 0, most -1);
-        System.out.println("\nUnsorted:");
+        System.out.println("Unsorted:");
         printArr(randIntArr1);
 
         int median1 = team11.challengeThree(randIntArr1);
+        time = System.currentTimeMillis();
         time = System.currentTimeMillis() - time;
-        System.out.println("Challenge Three Time Taken: " + time  + " Seconds");
+        System.out.println("\nChallenge Three Time Taken: " + time  + " Seconds");
         System.out.println("Median equals: " + median1);
 
-        System.out.println("Sorted:");
+        System.out.println("\nSorted:");
         printArr(randIntArr1);
+        System.out.println("");
 
         //Challenge Four
+        System.out.println("Challenge Four\n------------------------------------------");
+        int[][] randMultArr = randMultiArr(1000);
+        System.out.println("Unsorted:");
+        printMultiArr(randMultArr);
+        int median2 = team11.challengeFour(randMultArr);
 
+        time = System.currentTimeMillis();
+        time = System.currentTimeMillis() - time;
+        System.out.println("\nChallenge Four Time Taken: " + time  + " Seconds");
+        System.out.println("Median equals: " + median2);
+
+        System.out.println("\nSorted:");
+        printMultiArr(randMultArr);
+        System.out.println("");
 
         //Challenge Five
+        System.out.println("Challenge Five\n------------------------------------------");
+        Comparable[] randomComparableArr = randomComparableArr(10000);
+        Team11SortCompetition.bubbleComparableSort(randomComparableArr);
+        int indexNumber1 = team11.challengeFive(randomComparableArr,"fasff");
+
+        time = System.currentTimeMillis();
+        time = System.currentTimeMillis() - time;
+        System.out.println("Challenge Five Time Taken: " + time  + " Seconds");
+
+
+        if (indexNumber1 >= 0)
+            System.out.println("Index is found at: " + indexNumber);
+        else
+            System.out.println("Index not found.");
     }
-    public static int[] randomIntsArr(int count)
-    {
+
+    public static int[] randomIntsArr(int count) {
         int[] arr = new int[count];
-        for(int i = 0; i < arr.length; i++)
-        {
+        for(int i = 0; i < arr.length; i++) {
             arr[i] = (int)(Math.random()*10000);
         }
         return arr;
     }
-    public static String[] randomStringArr(int num, int length)
-    {
+    public static int [][] randMultiArr(int count){
+        int[][] arr =  new int [count][count];
+        for(int i=0; i < arr.length; i++) {
+            for(int j=0;j<arr[0].length;j++) {
+                arr[i][j] = (int) (Math.random()*100);
+            }
+        }
+        return arr;
+    }
+    public static String[] randomStringArr(int num, int length) {
         String [] arr = new String [num];
-        while(num>0)
-        {
+        while(num>0) {
             int i =0;
             String s = "";
-            while(i<length)
-            {
+            while(i<length) {
                 char c = (char)((Math.random()*26)+97);
                 s = s + c;
                 i++;
@@ -87,13 +126,27 @@ public class Runner {
         }
         return arr;
     }
-    public static void printArr(int[] arr)
+    public static Comparable[] randomComparableArr(int num) {
+        Comparable[] arr = new Comparable[num];
+        for (int i = 0; i < num; i++) {
+            SortCompetition temp = new Team11SortCompetition();
+            temp.addTime(Math.random() * (num + 1));
+            arr[i] = temp;
+        }
+        return arr;
+    }
+    public static void printArr(int[] arr) {
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void printStringArr(String[] arr) {
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void printMultiArr(int[][] arr)
+    {
+        System.out.println(Arrays.deepToString(arr));
+    }
+    public static void printComparableArr(Comparable[] arr)
     {
         System.out.println(Arrays.toString(arr));
     }
-    public static void printStringArr(String[] arr)
-    {
-        System.out.println(Arrays.toString(arr));
-    }
-
 }
--- src/Team11SortCompetition.java
+++ src/Team11SortCompetition.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Team11SortCompetition extends SortCompetition {
 
     public static void swapInt(int[] arr, int i, int j) {
@@ -6,8 +8,20 @@ public class Team11SortCompetition extends SortCompetition {
         arr[j] = temp;
     }
 
+    public static void swapInt(int[][] arr, int i, int j) {
+        int[] temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
     public static void swapString(String[] arr, int i, int j) {
         String temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void swapComparable(Comparable[] arr, int i, int j) {
+        Comparable temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
@@ -25,6 +39,7 @@ public class Team11SortCompetition extends SortCompetition {
         return i+1;
     }
 
+
     public static void quickSort(int[] arr,int left, int right) {
         if (left < right) {
             int pivot = partition(arr,left,right);
@@ -33,7 +48,38 @@ public class Team11SortCompetition extends SortCompetition {
             quickSort(arr,pivot + 1, right);
         }
     }
+    public static void quickSort(int[] arr,int left, int right, int [][] arr2) {
+        if (left < right) {
+            int pivot = partition(arr,left,right,arr2);
 
+            quickSort(arr, left,pivot - 1,arr2);
+            quickSort(arr,pivot + 1, right,arr2);
+        }
+    }
+    public static void insertionSort(int[] list1, int[][] arr) {
+        for (int i = 1; i < list1.length; i++) {
+            int temp = list1[i];
+            int j;
+            for (j = i - 1; j >= 0 && temp < list1[j]; j--) {
+                swapInt(arr,j+1,j);
+                list1[j + 1] = list1[j];
+            }
+            list1[j + 1] = temp;
+        }
+    }
+    public static int partition(int[] arr, int left, int right, int[][] arr2) {
+        int pivot = arr[right];
+        int i = left - 1;
+        for(int j  = left; j <right;j++) {
+            if(arr[j]<=pivot) {
+                i++;
+                swapInt(arr,i,j);
+                swapInt(arr2,i,j);
+            }
+        }
+        swapInt(arr,i+1,right);
+        return i+1;
+    }
     public static void bubbleSort(String[] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
@@ -43,27 +89,41 @@ public class Team11SortCompetition extends SortCompetition {
             }
         }
     }
+    public static void bubbleComparableSort(Comparable[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j].compareTo(arr[j+1]) > 0) {
+                    swapComparable(arr, j, j + 1);
+                }
+            }
+        }
+    }
+
+    public static int median(int[] arr)
+    {
+        int mid = 0;
+        if(arr.length % 2 == 0) {
+            mid = (arr[arr.length / 2 + 1] + arr[arr.length / 2]) / 2;
+        }
+        else
+        {
+            mid = arr[arr.length / 2];
+        }
+        return mid;
+    }
+
+
 
 
     public int challengeOne(int[] arr) {
         quickSort(arr,0,arr.length-1);
-        int mid = 0;
-        if(arr.length % 2 == 0)
-        for(int i = 0;i<arr.length/2;i++) {
-            mid = (arr[i] + arr[i+1])/2;
-        }
-        else {
-            for(int i = 0;i<arr.length/2;i++) {
-                mid = arr[i+1];
-            }
-        }
-        return mid;
+        return median(arr);
     }
 
     public int challengeTwo(String[] arr, String query) {
         bubbleSort(arr);
         for(int i = 0;i<arr.length-1;i++) {
-            if(arr[i].equals(query)) {
+            if(arr[i].compareTo(arr[i+1]) > 0) {
                 return i;
             }
         }
@@ -86,24 +146,23 @@ public class Team11SortCompetition extends SortCompetition {
     }
 
     public int challengeFour(int[][] arr) {
-        int median = 0;
-        int[] x = new int[arr.length];
+        int[] median = new int[arr.length];
 
         for(int i=0; i < arr.length; i++) {
-            challengeOne(arr[i]);
-            for(int j = 0; j < arr[i].length; j++)
-            {
-                median = challengeOne(arr[j]);
-            }
+            median[i] = challengeOne(arr[i]);
         }
-
-
-
-        return median;
+        insertionSort(median,arr);
+        return median(median);
     }
 
+
     public int challengeFive(Comparable[] arr, Comparable query) {
-        bubbleSort();
+        bubbleComparableSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(query)) {
+                return i;
+            }
+        }
         return -1;
     }
 
