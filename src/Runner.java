c3b4a9b5edaed0c59f5ee241e6d32f1ebc13d22b
BT_1N3_10 <cwu8959@bths.edu>
2018-12-05 11:19:35 -0500

    update

--- src/Runner.java
+++ src/Runner.java
@@ -9,18 +9,13 @@ public class Runner {
         int[] challenge3Arr = Utilities.mostlySortedArr(75000,25000);
         int[][] challenge4Arr = Utilities.gen2dInt(10, 10);
         //Thingy[] challenge5Arr = Utilities.genComparableArr(10000);
-        Thingy[] thingyArr = new Thingy[10000];
-        for (int i = 0; i < 10000; i++) {
-            thingyArr[i] = new Thingy();
-        }
-        Thingy query = thingyArr[(int)Math.random()*thingyArr.length];
-        System.out.println("Challenge 5: " + sortComp.challengeFive(thingyArr, query));
+
         //Challenge 1
         //System.out.println(Arrays.toString(challenge1Arr));
         long startTime = System.currentTimeMillis();
         System.out.println("Challenge 1 Median: " + sortComp.challengeOne(challenge1Arr));
         long endTime = System.currentTimeMillis();
-        long duration = (endTime - startTime);
+        long duration = endTime - startTime;
         //System.out.println(Arrays.toString(challenge1Arr));
         /*System.out.println("Challenge 1 time: " + duration + "ms.\n");
         //Challenge 3
@@ -39,6 +34,15 @@ public class Runner {
         duration = (endTime - startTime);
         System.out.println("Challenge 4 time: " + duration + "ms.\n");
         System.out.println(Arrays.deepToString(challenge4Arr));
-
+        Thingy[] thingyArr = new Thingy[10000];
+        for (int i = 0; i < 10000; i++) {
+            thingyArr[i] = new Thingy();
+        }
+        Thingy query = thingyArr[(int)Math.random()*thingyArr.length];
+        long startTime5 = System.currentTimeMillis();
+        System.out.println("Challenge 5: " + sortComp.challengeFive(thingyArr, query));
+        long endTime5 = System.currentTimeMillis();
+        long duration5 = (endTime5 - startTime5);
+        System.out.println("Challenge 5 time: " + duration5 + "ms.\n");
     }
 }
