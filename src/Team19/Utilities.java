2cf6142b6ab432bbb4ee65ea3e4472175c24661e
Edmund Wu <fangkazuto@gmail.com>
2018-12-07 18:06:26 -0500

    Team19: median fix

--- src/Team19/Utilities.java
+++ src/Team19/Utilities.java
@@ -2,12 +2,12 @@ package Team19;
 
 public class Utilities {
     public static int calculateMedian(int[] arr) {
-        return arr.length % 2 == 1 ? arr[arr.length / 2 + 1] : (arr[arr.length / 2] + arr[arr.length / 2 + 1]) / 2;
+        return arr.length % 2 == 1 ? arr[arr.length / 2] : (arr[arr.length / 2] + arr[arr.length / 2 - 1]) / 2;
     }
 
     public static int calculateMedian(int[][] arr) {
-        float mid0 = calculateMedian(arr[arr.length / 2 + 1]);
-        float mid1 = arr.length % 2 == 1 ? mid0 : calculateMedian(arr[arr.length / 2]);
-        return (int) ((mid0 + mid1) / 2);
+        int mid0 = calculateMedian(arr[arr.length / 2]);
+        int mid1 = arr.length % 2 == 1 ? mid0 : calculateMedian(arr[arr.length / 2 - 1]);
+        return (mid0 + mid1) / 2;
     }
 }
